3f8114ba92205324492f7c4d8dcd7ce59693a0bf
Iqbal <iqbalzafar909700@gmail.com>
2021-01-01 10:56:23 +0530

    JAVASCRIPT (DOM && CSSOM)

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("hello");
+document.getElementById("heading").style.color = "green";
+function clicked() {
+    console.log("I am clicked");
+    document.getElementById("heading").style.backgroundColor = "red";
+    document.getElementById("heading").style.color = "white";
+}
